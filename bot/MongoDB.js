d5372262a0419a316113865093f90b91a2f3dc8f
pxlatedtraveler <lighthalzenxii@gmail.com>
2022-03-31 16:59:08 -0700

    Working connection and reading of data on mongoDB.

--- bot/MongoDB.js
+++ bot/MongoDB.js
@@ -4,12 +4,27 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const token = process.env.ATLASADMIN;
 
-const uri = `mongodb+srv://admin:${token}@dce.dmkhr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-dbClient.connect(err => {
-    if (err) console.log(err);
-    const collection = dbClient.db('test').collection('devices');
-    // perform actions on the collection object
-    console.log(collection);
-    dbClient.close();
-});
+const uri = `mongodb+srv://admin:${token}@dce.dmkhr.mongodb.net/dce?retryWrites=true&w=majority?directConnection=true`;
+
+main();
+
+async function main() {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+    try {
+        await client.connect();
+        await listDatabases(client);
+    }
+    catch (e) {
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client) {
+    const databaseList = await client.db().admin().listDatabases();
+
+    databaseList.databases.forEach(db => console.log(db.name));
+}
